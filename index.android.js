6f380b1a60e831ebb31a70ca2e5167c95de8dd10
Xuan Huy Dev <nguyenxuanhuy102@gmail.com>
2017-09-13 01:57:53 +0700

    flatlist OK

--- index.android.js
+++ index.android.js
@@ -1,45 +1,34 @@
 import React, { Component } from 'react';
-import {  AppRegistry,  StyleSheet,  Text,  View} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 const Realm = require('realm');
-import RNFS  from 'react-native-fs';
-
-// const XeMaySchema = {
-//   name: 'ffsdfsfsd',
-//   properties: {
-//     key:'int',
-//     ten_loi: 'string',
-//     noi_dung: 'string',
-//     muc_phat: 'string',
-//     phat_bo_sung: 'string',
-//     dieu_khoan: 'string',
-//     doi_tuong: 'string'
-//   }
-// };
-
-const Hotline = {
-  name: 'DuongDayNong',
+import RNFS from 'react-native-fs';
+import Hotline from './schema/schema.js'
+const LoiSchema = {
+  name: 'LoiOto',
   properties: {
-    key:'int',
-    ten_tinh: 'string',
-    sdt_tinh: 'string'
+    key: 'int',
+    ten_loi: 'string',
+    noi_dung: 'string',
+    muc_phat: 'string',
+    phat_bo_sung: 'string',
+    dieu_khoan: 'string',
+    doi_tuong: 'string'
   }
 };
+console.log(Hotline)
 
 
-// RNFS.copyFileAssets('huy.realm', RNFS.DocumentDirectoryPath + '/huy.realm')
+// RNFS.copyFileAssets('hotline', RNFS.DocumentDirectoryPath + '/hotline')
 //   .then(() => {
 //     const realm = new Realm({
-//       path: RNFS.DocumentDirectoryPath + '/huynew.realm',
-//       schema: [CarSchema]
-//     })
-//     realm.write(() => {
-//       realm.create('Item', { key: 22, tenLoi: 'day la ten loi', mucPhat: 'day la muc phat' })
+//       path: RNFS.DocumentDirectoryPath + '/hotline',
+//       schema: [Hotline]
 //     })
 
-//     const cars = realm.objects('Item')
+//     const cars = realm.objects('DuongDayNong').slice(5, 10)
 //     console.log('====================')
-//     console.log('lengh: ' + cars.length)
-//     console.log('====================')
+//     console.log(cars)
+//     console.log('=================================================')
 //   })
 
 
@@ -50,40 +39,66 @@ export default class fs extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       realm: null,
+      mangHotline: [],
+      begin:0,
+      end:5
 
     };
   }
 
 
+
   componentDidMount() {
-    fetch("http://192.168.8.2/xuphat/main.php")
-      .then((data) => data.json())
-      .then((dataJson) => { dataJson.forEach((item) => { 
-        const realm = new Realm({ schema:[Hotline],path:'hotline' })
-        realm.write( ()=>{
-          realm.create('DuongDayNong', item)
-        } )
-        this.setState({
-          realm:realm
+    RNFS.copyFileAssets('xeoto', RNFS.DocumentDirectoryPath + '/xeoto')
+      .then(() => {
+        const realm = new Realm({
+          path: RNFS.DocumentDirectoryPath + '/xeoto',
+          schema: [LoiSchema]
         })
-       })
+        
+        items = realm.objects('LoiOto').slice(this.state.begin, this.state.end)
+        this.setState({
+          mangHotline: [...this.state.mangHotline, ...items],
+          begin:this.state.begin+5,
+          end:this.state.end+5
+        })
       })
-      .catch((e) => { console.log(e) })
+  }
+  EndReach = () => {
+
+    RNFS.copyFileAssets('xeoto', RNFS.DocumentDirectoryPath + '/xeoto')
+      .then(() => {
+        const realm = new Realm({
+          path: RNFS.DocumentDirectoryPath + '/xeoto',
+          schema: [LoiSchema]
+        })
+        items = realm.objects('LoiOto').slice(this.state.begin, this.state.end)
+
+        if (items.length > 0) {
+          this.setState({
+            mangHotline: [...this.state.mangHotline, ...items],
+            begin:this.state.begin+5,
+            end:this.state.end+5
+          })
+        }else{
+          return 0
+        }
+      })
+
   }
 
-
   render() {
-    const info = this.state.realm ? 'num: '+ this.state.realm.objects('DuongDayNong').length :'loading'
-
 
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {info}
-        </Text>
+        <FlatList
+          data={this.state.mangHotline}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}---{item.ten_loi}--{item.muc_phat}</Text>}
+          onEndReachedThreshold={0.2}
+          onEndReached={() => { this.EndReach() } }
+          />
       </View>
     );
   }
@@ -93,19 +108,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  item: {
+    backgroundColor: 'pink',
+    padding: 35,
+    margin: 3,
+
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('fs', () => fs);
---
+++ schema/schema.js
@@ -0,0 +1,8 @@
+export const Hotline = {
+  name: 'DuongDayNong',
+  properties: {
+    key: 'int',
+    ten_tinh: 'string',
+    sdt_tinh: 'string'
+  }
+}

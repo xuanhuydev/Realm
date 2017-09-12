ca0436a053bc9b1e55b562c3bc6ed23154ea424a
Xuan Huy Dev <nguyenxuanhuy102@gmail.com>
2017-09-12 10:17:26 +0700

    fetch data vao realm

--- index.android.js
+++ index.android.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import {  AppRegistry,  StyleSheet,  Text,  View} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, FlatList } from 'react-native';
 const Realm = require('realm');
-import RNFS  from 'react-native-fs';
-const CarSchema = {
-  name: 'Car',
+import RNFS from 'react-native-fs';
+const ItemSchema = {
+  name: 'Item',
   properties: {
-    model: 'string',
+    key: 'int',
+    tenLoi: 'string',
+    mucPhat: 'string'
   }
 };
 
-RNFS.copyFileAssets('huy.realm',RNFS.DocumentDirectoryPath + '/huy.realm')
-.then( ()=>{
-  const realm = new Realm({
-    path:RNFS.DocumentDirectoryPath + '/huy.realm',
-    schema:[CarSchema]
-  })
+// RNFS.copyFileAssets('huy.realm',RNFS.DocumentDirectoryPath + '/huy.realm')
+// .then( ()=>{
+//   const realm = new Realm({
+//     path:RNFS.DocumentDirectoryPath + '/huy.realm',
+//     schema:[CarSchema]
+//   })
 
-  const people = realm.objects('Car')
-  console.log('====================')
-  console.log('lengh: '+people.length)
-  console.log('====================')
-} )
+//   const people = realm.objects('Car')
+//   console.log('====================')
+//   console.log('lengh: '+people.length)
+//   console.log('====================')
+// } )
 
 
 
@@ -28,25 +30,40 @@ export default class fs extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { realm: null };
+    this.state = {
+      realm: null,
+      mang: []
+    };
   }
 
 
-  componentWillMount() {
-    Realm.open({
-      schema: [CarSchema],
-      path:'huy.realm'
-    }).then(realm => {
-      realm.write(() => {
-        realm.create('Car', {model: 'Van Huy'});
-      });
-      this.setState({ realm });
-    });
+  componentDidMount() {
+    fetch("http://192.168.8.2/xuphat/main.php")
+      .then((data) => data.json())
+      .then((dataJson) => {
+
+        dataJson.forEach((item) => {
+          Realm.open({
+            schema: [ItemSchema],
+            path: RNFS.DocumentDirectoryPath + '/huynew.realm'
+          }).then(realm => {
+            realm.write(() => {
+              realm.create('Item', item);
+            });
+            this.setState({ realm });
+          });
+        })
+        this.setState({
+          mang: dataJson
+        })
+      })
+      .catch((e) => { console.log(e) })
   }
+
 
   render() {
     const info = this.state.realm
-      ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Car').length
+      ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Item').length
       : 'Loading...';
 
     return (
@@ -54,6 +71,10 @@ export default class fs extends Component {
         <Text style={styles.welcome}>
           {info}
         </Text>
+        <FlatList
+          data={this.state.mang}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+          />
       </View>
     );
   }

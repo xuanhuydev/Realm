84496e1092702ec22be6269a7868f4c75f3595f3
Xuan Huy Dev <nguyenxuanhuy102@gmail.com>
2017-09-12 17:57:23 +0700

    add xong realm

--- index.android.js
+++ index.android.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, FlatList } from 'react-native';
+import {  AppRegistry,  StyleSheet,  Text,  View} from 'react-native';
 const Realm = require('realm');
-import RNFS from 'react-native-fs';
-const ItemSchema = {
-  name: 'Item',
+import RNFS  from 'react-native-fs';
+
+// const XeMaySchema = {
+//   name: 'ffsdfsfsd',
+//   properties: {
+//     key:'int',
+//     ten_loi: 'string',
+//     noi_dung: 'string',
+//     muc_phat: 'string',
+//     phat_bo_sung: 'string',
+//     dieu_khoan: 'string',
+//     doi_tuong: 'string'
+//   }
+// };
+
+const Hotline = {
+  name: 'DuongDayNong',
   properties: {
-    key: 'int',
-    tenLoi: 'string',
-    mucPhat: 'string'
+    key:'int',
+    ten_tinh: 'string',
+    sdt_tinh: 'string'
   }
 };
 
-RNFS.copyFileAssets('huy.realm', RNFS.DocumentDirectoryPath + '/huy.realm')
-  .then(() => {
-    const realm = new Realm({
-      path: RNFS.DocumentDirectoryPath + '/huynew.realm',
-      schema: [ItemSchema]
-    })
-    realm.write(() => {
-      realm.create('Item', { key: 22, tenLoi: 'day la ten loi', mucPhat: 'day la muc phat' })
-    })
 
-    const cars = realm.objects('Item')
-    console.log('====================')
-    console.log('lengh: ' + cars.length)
-    console.log('====================')
-  })
+// RNFS.copyFileAssets('huy.realm', RNFS.DocumentDirectoryPath + '/huy.realm')
+//   .then(() => {
+//     const realm = new Realm({
+//       path: RNFS.DocumentDirectoryPath + '/huynew.realm',
+//       schema: [CarSchema]
+//     })
+//     realm.write(() => {
+//       realm.create('Item', { key: 22, tenLoi: 'day la ten loi', mucPhat: 'day la muc phat' })
+//     })
+
+//     const cars = realm.objects('Item')
+//     console.log('====================')
+//     console.log('lengh: ' + cars.length)
+//     console.log('====================')
+//   })
+
+
 
 
 
@@ -33,6 +50,7 @@ export default class fs extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       realm: null,
 
@@ -43,28 +61,29 @@ export default class fs extends Component {
   componentDidMount() {
     fetch("http://192.168.8.2/xuphat/main.php")
       .then((data) => data.json())
-      .then((dataJson) => {
-
-        dataJson.forEach((item) => {
-          console.log('ahihi')
+      .then((dataJson) => { dataJson.forEach((item) => { 
+        const realm = new Realm({ schema:[Hotline],path:'hotline' })
+        realm.write( ()=>{
+          realm.create('DuongDayNong', item)
+        } )
+        this.setState({
+          realm:realm
         })
+       })
       })
       .catch((e) => { console.log(e) })
   }
 
 
   render() {
-    const info = this.state.realm
-      ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Item').length
-      : 'Loading...';
+    const info = this.state.realm ? 'num: '+ this.state.realm.objects('DuongDayNong').length :'loading'
+
 
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           {info}
-          huy
         </Text>
-
       </View>
     );
   }
